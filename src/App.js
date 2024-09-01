25a07613f922300dbdee835fd19ff7120354ede7
Aqua <86239876+Aqua-123@users.noreply.github.com>
2024-09-01 14:50:56 +0530

    Remove redundancy

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Signup from './components/Signup/Signup';
-import Signin from './components/Signin/Signin';
 import SpreadsheetPage from './components/Spreadsheet/Spreadsheet';
 import SpreadsheetsPage from './components/Spreadsheets/SpreadsheetsPage';
 import 'handsontable/dist/handsontable.full.min.css';
 import { registerAllModules } from 'handsontable/registry';
-import Providers from './Providers'; // Import the Providers
-// import SigninPage from './components/NewSigninPage';
-import Signup from './components/TestSignUp';
+import Providers from './Providers';
+import Signup from './components/SignUp';
 
 registerAllModules();
 
